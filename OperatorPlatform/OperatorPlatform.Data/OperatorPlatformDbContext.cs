15065cc663592d6703c6cc63a46400f44f1c96c7
Francisco <fonfonferi@gmail.com>
2021-09-05 21:29:48 -0700

    DB Context

---
+++ OperatorPlatform/OperatorPlatform.Data/OperatorPlatformDbContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OperatorPlatform.Data
+{
+    public class OperatorPlatformDbContext : DbContext
+    {
+        public string ConnectionString { get; set; }
+        #region DbSets
+        #endregion
+        public OperatorPlatformDbContext(DbContextOptions<OperatorPlatformDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
