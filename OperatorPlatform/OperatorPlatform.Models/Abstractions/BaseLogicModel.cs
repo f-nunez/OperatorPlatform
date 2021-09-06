eb29703b807c7b73faac3e03ae9bda3d546b63a9
Francisco <fonfonferi@gmail.com>
2021-09-05 21:58:02 -0700

    Abstractions on models

---
+++ OperatorPlatform/OperatorPlatform.Models/Abstractions/BaseLogicModel.cs
@@ -0,0 +1,8 @@
+﻿namespace OperatorPlatform.Models.Abstractions
+{
+    public abstract class BaseLogicModel : BaseModel
+    {
+        public int Id { get; set; }
+        public bool Active { get; set; }
+    }
+}
