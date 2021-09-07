01ca10cdfa6d722764e4aada2a33d71d619111ac
Francisco <fonfonferi@gmail.com>
2021-09-06 23:46:29 -0700

    Adding Mappings to DataBaseContext

--- OperatorPlatform/OperatorPlatform.Data/OperatorPlatformDbContext.cs
+++ OperatorPlatform/OperatorPlatform.Data/OperatorPlatformDbContext.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OperatorPlatform.Mappings;
+using OperatorPlatform.Models;
 
 namespace OperatorPlatform.Data
 {
@@ -6,6 +8,12 @@ namespace OperatorPlatform.Data
     {
         public string ConnectionString { get; set; }
         #region DbSets
+        public DbSet<Alert> Alerts { get; set; }
+        public DbSet<Bar> Bars { get; set; }
+        public DbSet<Exchange> Exchanges { get; set; }
+        public DbSet<Indicator> Indicators { get; set; }
+        public DbSet<Operation> Operations { get; set; }
+        public DbSet<Ticker> Tickers { get; set; }
         #endregion
         public OperatorPlatformDbContext(DbContextOptions<OperatorPlatformDbContext> options) : base(options)
         {
@@ -22,6 +30,12 @@ namespace OperatorPlatform.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfiguration(new AlertMapping());
+            modelBuilder.ApplyConfiguration(new BarMapping());
+            modelBuilder.ApplyConfiguration(new ExchangeMapping());
+            modelBuilder.ApplyConfiguration(new IndicatorMapping());
+            modelBuilder.ApplyConfiguration(new OperationMapping());
+            modelBuilder.ApplyConfiguration(new TickerMapping());
         }
     }
 }
