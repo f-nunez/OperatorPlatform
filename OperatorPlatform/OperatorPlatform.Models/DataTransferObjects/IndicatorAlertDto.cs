c4096fb183f44cd6dd4467af3be65247619984f2
Francisco <fonfonferi@gmail.com>
2021-09-19 01:19:11 -0700

    Add IndicatorAlertDto

---
+++ OperatorPlatform/OperatorPlatform.Models/DataTransferObjects/IndicatorAlertDto.cs
@@ -0,0 +1,12 @@
+﻿namespace OperatorPlatform.Models.DataTransferObjects
+{
+    public class IndicatorAlertDto
+    {
+        public string IndicatorDescription { get; set; }
+        public string IndicatorName { get; set; }
+        public string OperationType { get; set; }
+        public string TickerName { get; set; }
+        public string Time { get; set; }
+        public string TimeFrame { get; set; }
+    }
+}
