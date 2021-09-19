cad1287866a96e9f9a11bf02d0ca2c29ea55223e
Francisco <fonfonferi@gmail.com>
2021-09-19 01:18:58 -0700

    Add BarAlertDto

---
+++ OperatorPlatform/OperatorPlatform.Models/DataTransferObjects/BarAlertDto.cs
@@ -0,0 +1,21 @@
+﻿namespace OperatorPlatform.Models.DataTransferObjects
+{
+    public class BarAlertDto
+    {
+        public string BollingerLowerBandFirst { get; set; }
+        public string BollingerLowerBandSecond { get; set; }
+        public string BollingerLowerBandThird { get; set; }
+        public string BollingerUpperBandFirst { get; set; }
+        public string BollingerUpperBandSecond { get; set; }
+        public string BollingerUpperBandThird { get; set; }
+        public string Close { get; set; }
+        public string ExchangeBarId { get; set; }
+        public string High { get; set; }
+        public string Low { get; set; }
+        public string MovingAverage { get; set; }
+        public string Open { get; set; }
+        public string TickerName { get; set; }
+        public string Time { get; set; }
+        public string Volume { get; set; }
+    }
+}
