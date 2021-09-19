8b3af5360b56400f10bbe4e73754b28bf7ea6f71
Francisco <fonfonferi@gmail.com>
2021-09-19 01:20:06 -0700

    Controller actions for BarAlert and IndicatorAlert dtos

--- OperatorPlatform/OperatorPlatform.API/Controllers/AlertController.cs
+++ OperatorPlatform/OperatorPlatform.API/Controllers/AlertController.cs
@@ -8,9 +8,16 @@ namespace OperatorPlatform.API.Controllers
     public class AlertController : ControllerBase
     {
         [HttpPost]
-        public IActionResult PostAddAlert(AlertDto alertDto)
+        public IActionResult PostBarAlert(BarAlertDto barAlert)
         {
-            var resultMessage = $"The ticker that comes from alert is {alertDto.Ticker.ToUpper()}";
+            var resultMessage = $"The ticker that comes from BarAlert is {barAlert.TickerName.ToUpper()}";
+            return Ok(resultMessage);
+        }
+
+        [HttpPost]
+        public IActionResult PostIndicatorAlert(IndicatorAlertDto indicatorAlert)
+        {
+            var resultMessage = $"The ticker that comes from IndicatorAlert is {indicatorAlert.TickerName.ToUpper()}";
             return Ok(resultMessage);
         }
     }
