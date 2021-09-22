72b51d0645696c638892275826925977ee7cb38d
Francisco <fonfonferi@gmail.com>
2021-09-21 21:58:26 -0700

    IndicatorAlertdto properties type updated.

--- OperatorPlatform/OperatorPlatform.Models/DataTransferObjects/IndicatorAlertDto.cs
+++ OperatorPlatform/OperatorPlatform.Models/DataTransferObjects/IndicatorAlertDto.cs
@@ -1,12 +1,15 @@
-﻿namespace OperatorPlatform.Models.DataTransferObjects
+﻿using OperatorPlatform.Models.Enums;
+using System;
+
+namespace OperatorPlatform.Models.DataTransferObjects
 {
     public class IndicatorAlertDto
     {
         public string IndicatorDescription { get; set; }
         public string IndicatorName { get; set; }
-        public string OperationType { get; set; }
+        public OperationType OperationType { get; set; }
         public string TickerName { get; set; }
-        public string Time { get; set; }
-        public string TimeFrame { get; set; }
+        public DateTime? Time { get; set; }
+        public TimeFrame TimeFrame { get; set; }
     }
 }
